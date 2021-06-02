d7f4dc62841c6677678d8056fef9c6a3d4dc8f6e
Kstives <kareylstivers@gmail.com>
2021-06-02 06:42:55 -0700

    Adding lesson five review files and easy5 small problems file

--- lesson-five/lesson-review.js
+++ lesson-five/lesson-review.js
@@ -1,3 +1,30 @@
 // review of Lesson five concepts
 
-// Introduction
+// Sorting
+
+// How would you sort the following array by the lengths of each word?
+
+let words = ['go', 'ahead', 'and', 'jump'];
+
+words.sort((a, b) => a.length - b.length);
+
+console.log(words);
+
+let scores = [[3, 6, 4], [6, 8, 9], [1, 4, 2]];
+scores.sort((a, b) => {
+  let totalAScore = a.reduce((number, next) => number + next);
+  let totalBScore = b.reduce((number, next) => number + next);
+
+  return totalAScore - totalBScore;
+});
+
+console.log(scores);
+
+// SHALLOW COPIES
+let arr = [{ a: 'foo'}, {bar: 'bar'}, {c: 'baz'}];
+let copyOfArr = [...arr];
+
+copyOfArr[1].d = 'qux';
+
+console.log(arr);
+console.log(copyOfArr);
--- small-problems-exercises/easy5/after-midnight-2.js
+++ small-problems-exercises/easy5/after-midnight-2.js
@@ -27,17 +27,31 @@ PEDAC PROCESS
 
 P: UNDERSTAND THE PROBLEM
   EXPECTED INPUT AND OUTPUT
-    Input:
-    Output:
+    Input: string argument of a time of day in 24 hour format (ex. "00:00" or
+    "12:34")
+    Output: The number of minutes equal to that time of day (686 for "12:34"
+    if inputed into beforeMidnight function)
 
 
 TERMS OF THE PROBLEM DOMAIN
   Terms of the problem domain:
-    *
+    * Minutes in a day = 1440
+    * positive num = time after midnight = counting time forwards from 00:00
+    * negative num = time before midnight = counting time backwards from 00:00
+    * "24:00/00:00" = midnight = 0
+    * 24 hour format = first set of numbers 00 - 24, second set of numbers
+    00 - 59
 
 IDENTIFY THE RULES (EXPLICIT & IMPLICIT REQUIREMENTS)
 
-  1.
+  1. One function: beforeMidnight - calculates time backwards from midnight
+  ("24:00" - minutes)
+  2. One function: afterMidnight - calculates time forwards from midnight
+  ("00:00" + minutes)
+  3. the input is a string, the output is a number
+  4. The range of the return value has to be in 0-1439
+  5. Cannot use JavaScript's Date class methods
+
 
   Clarifying Questions:
     1.
@@ -47,16 +61,26 @@ IDENTIFY THE RULES (EXPLICIT & IMPLICIT REQUIREMENTS)
  E: EXAMPLES/TEST CASES
  (based on the above rules and edge cases)
    EXAMPLE ONE
-    Input:
-    Output:
+    Input: afterMidnight("12:34")
+    Output: (12 x 60) + 34 = 754
+
+    EXAMPLE TWO
+    Input: beforeMidnight("12:34")
+    Output: (11 x 60) - 26 = ((12 - 1) x 60) - (60 - 34) = 686
 
 D: DATA STRUCTURE
 
 A: ALGORITHM
-
+AfterMidnight formula: (Hour * 60) + Minutes
+Before Midnight formula: ((Hour - 1) * 60) - (60 - Minutes)
 START
-
-  1.
+    AFTERMIDNIGHT
+  1. Retrieve the hours from the input and SET as hours
+  2. Retrieve the minutes from the input and SET as minutes
+  3. Convert hours to a number
+  4. Convert minutes to a number
+  5. Multiply the hours by 60 plus 5 and SET as numOfMinutes
+  6. Return numOfMinutes
 
 END
 
@@ -73,3 +97,35 @@ END
  */
 
 // C: CODE WITH INTENT
+
+function afterMidnight(timeString) {
+  let hours = Number(timeString.slice(0, 2));
+  let minutes = Number(timeString.slice(3));
+  
+
+  if ((hours && minutes === 0) || (hours === 24)) {
+    return 0;
+  }
+
+  return (hours * 60) + minutes;
+
+}
+
+function beforeMidnight(timeString) {
+  let hours = Number(timeString.slice(0, 2));
+  let minutes = Number(timeString.slice(3));
+  
+  if ((hours === 0 && minutes === 0) || hours === 24) {
+    return 0;
+  }
+
+  return ((hours - 1) * 60) + (60 - minutes);
+
+}
+
+console.log(afterMidnight("00:00") === 0);
+console.log(beforeMidnight("00:00") === 0);
+console.log(afterMidnight("12:34") === 754);
+console.log(beforeMidnight("12:34") === 686);
+console.log(afterMidnight("24:00") === 0);
+console.log(beforeMidnight("24:00") === 0);
